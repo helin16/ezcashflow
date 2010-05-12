4d1058e8fa77f633e98019c5e77a75ff3f682c43
helin16 <helin16@dcc96739-f24e-c616-75c6-04b6b03958bd>
2010-05-12 09:58:27 +0000

    migrate to Pradosoft

--- Web/index.php
+++ Web/index.php
@@ -1,49 +1,16 @@
 <?php
-require(dirname(__FILE__) . '/bootstrap.php');
+define('PRADO_CHMOD',0755);
+set_include_path('F:\mySandBox\ezcashflow\Web' . PATH_SEPARATOR . 'F:\mySandBox\ezcashflow\Web');
+$basePath=dirname(__FILE__);
+$assetsPath=$basePath.'/assets';
+$runtimePath=$basePath.'/protected/runtime';
 
-date_default_timezone_set('Australia/Melbourne');
+if(!is_writable($assetsPath))
+	die("Please make sure that the directory $assetsPath is writable by Web server process.");
+if(!is_writable($runtimePath))
+	die("Please make sure that the directory $runtimePath is writable by Web server process.");
 
-session_start();  
-if(isset($_SERVER["REQUEST_URI"]) && trim($_SERVER["REQUEST_URI"])!="/")
-{
-	if(strstr($_SERVER["REQUEST_URI"],"/post/"))
-	{
-		$vars = explode("/",$_SERVER["REQUEST_URI"]);
-		$serviceName = $vars[2];
-		$method = $vars[3];
-		
-		try
-		{
-			$service = new $serviceName();
-		}
-		catch(Exception $e)
-		{
-			die("Service $service don't exsits!");
-		}
-		
-		try
-		{
-			echo $service->$method($_POST);
-		}
-		catch(Exception $e)
-		{
-			die("Error calling method $method on service $serviceName: ".$e->getMessage());
-		}
-	}
-	else
-	{
-		if(System::getUser() instanceof UserAccount)
-		{
-			$vars = explode("/",$_SERVER["REQUEST_URI"]);
-			echo WapInterface::$vars[1]($vars);
-		}
-		else 
-			header("Location: /");
-	}
-}
-else 
-{
-	echo WapInterface::defaultPage(!System::getUser() instanceof UserAccount);
-}
-
+require 'bootstrap.php';
+$application=new TApplication;
+$application->run();
 ?>
