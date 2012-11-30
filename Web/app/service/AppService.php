ad0219f25bda87ebd488c168ecb04a81214d3319
helin16@gmail.com <helin16@gmail.com@dcc96739-f24e-c616-75c6-04b6b03958bd>
2012-11-30 03:46:04 +0000

    changed the WebLogin screen

--- Web/app/service/AppService.php
+++ Web/app/service/AppService.php
@@ -1,5 +1,38 @@
 <?php
 class AppService
 {
+	public function run()
+	{
+		//authenticate the user
+		if(!isset($_REQUEST["user"]))
+			throw new Exception("Invalid user info!");
+		$this->_authenticateUser($_REQUEST["user"]);
+			
+		$methods = explode(".", (isset($_REQUEST["method"]) ? trim($_REQUEST["method"]) : ''));
+		$serviceName = isset($methods[0]) ? trim($methods[0]) : '';
+		$func = isset($methods[1]) ? trim($methods[1]) : '';
+			
+		if(!isset($serviceName) || $serviceName === '')
+			throw new Exception("Empty Service!");
+		if (!class_exists($serviceName = "App" . ucfirst($serviceName) . "Service"))
+			throw new Exception("Service Unknown!");
+		$service = new $serviceName();
+			
+		if(!isset($func) || $func === '')
+			throw new Exception("Empty Function!");
+		if (!method_exists($service,$func))
+			throw new Exception("Requested Method Unknown!");
+			
+		return $service->$func($_REQUEST);
+	}
 	
+	private function _authenticateUser($userParam)
+	{
+		$userService = new UserAccountService();
+		$userAccount = $userService->getUserByUsernameAndPassword(isset($userParam['username']) ? $userParam['username'] : '', isset($userParam['password']) ? $userParam['password'] : '', false, false);
+		if(!$userAccount instanceof UserAccount)
+			throw new Exception("Invalid UserAccount!");
+	
+		Core::setUser($userAccount);
+	}
 }
