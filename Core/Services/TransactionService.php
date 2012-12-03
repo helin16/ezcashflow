e6c0a1b5f9d8e6941eed210dfe51921cb61b25f1
helin16@gmail.com <helin16@gmail.com@dcc96739-f24e-c616-75c6-04b6b03958bd>
2012-12-03 10:45:10 +0000

    added PHPDoc onto TransactionService

--- Core/Services/TransactionService.php
+++ Core/Services/TransactionService.php
@@ -1,33 +1,58 @@
 <?php
+/**
+ * Transaction service
+ * 
+ * @package    Core
+ * @subpackage Service
+ * @author     lhe<helin16@gmail.com>
+ *
+ */
 class TransactionService extends BaseService
 {
+    /**
+     * constructor
+     */
 	public function __construct()
 	{
 		parent::__construct("Transaction");
 	}
-	
+	/**
+	 * Transfer Money
+	 * @param AccountEntry $fromAccount The from account of the transaction
+	 * @param AccountEntry $toAccount   The to account of the transaction
+	 * @param string       $value       The value of the transaction
+	 * @param string       $comments    The comments of the transaction
+	 * 
+	 * @throws Exception
+	 * @return Transaction The new transaction
+	 * 
+	 */
 	public function transferMoney(AccountEntry $fromAccount,AccountEntry $toAccount,$value,$comments="")
 	{
 		$comments = htmlentities($comments);
 		$accountService = new AccountEntryService();
-		
 		if(!is_numeric($value))
 			throw new Exception("Invalid value to spend!");
-			
 		if($fromAccount->getId()==$toAccount->getId())
 			throw new Exception("Can't make transaction between the same account!");
-			
-		//if($fromAccount->getValue()<$value && $fromAccount->getRoot()->getId()==1)
-			//throw new Exception("You don't have enough money on this account to spend!");
-		
 		$transation = new Transaction();
 		$transation->setFrom($fromAccount);
 		$transation->setTo($toAccount);
 		$transation->setValue($value);
 		$transation->setComments($comments);
 		$this->save($transation);
+		return $transation;
 	}
-	
+	/**
+	 * earn Money
+	 * @param AccountEntry $fromAccount The from account of the transaction
+	 * @param AccountEntry $toAccount   The to account of the transaction
+	 * @param string       $value       The value of the transaction
+	 * @param string       $comments    The comments of the transaction
+	 * 
+	 * @throws Exception
+	 * @return Transaction The new transaction
+	 */
 	public function earnMoney(AccountEntry $toAccount,AccountEntry $fromAccount,$value,$comments="")
 	{
 		$comments = htmlentities($comments);
@@ -49,9 +74,19 @@ class TransactionService extends BaseService
 		$transation1->setValue($value);
 		$transation1->setComments($comments);
 		$this->save($transation1);
+		return array($transation, $transation1);
 	}
-
-	public function getSumOfExpenseBetweenDates($startDate,$endDate,$accountTypeId=4,$excludePosition='')
+    /**
+     * get the sum of the expense between dates
+     * 
+     * @param string $startDate       The start date
+     * @param string $endDate         The end date
+     * @param Int    $accountTypeId   The account root Id
+     * @param string $excludePosition The excluding account entries' position
+     * 
+     * @return double
+     */
+	public function getSumOfExpenseBetweenDates($startDate, $endDate, $accountTypeId = 4,$excludePosition = '')
 	{
 		$qry = "select sum(t.value) as sum 
 				from transaction t 
@@ -60,12 +95,21 @@ class TransactionService extends BaseService
 				and acc.rootId = $accountTypeId 
 				".($excludePosition=='' ? '' : " and acc.accountNumber not like '$excludePosition%'")."
 				and t.created >='$startDate' and t.created<'$endDate'";
-
 		$results = Dao::getResultsNative($qry,array(),PDO::FETCH_ASSOC);
-		return $results[0]["sum"];
+		return round($results[0]["sum"], 2);
 	}
-	
-	public function getTopExpenses($rootId=4,$noOfItems=4,$startDate='1790-01-01 00:00:00',$endDate="9999-12-31 23:59:59",$excludingIds=array())
+	/**
+	 * Getting the top expenses
+	 * 
+	 * @param Int    $rootId      The root id of the expense account entries
+	 * @param Int    $noOfItems   How many items
+	 * @param string $startDate   The start date
+	 * @param string $endDate     The end date
+	 * @param string $excludingIds The excluding account entries' position
+     * 
+     * @return double
+	 */
+	public function getTopExpenses($rootId = 4, $noOfItems = 4,$startDate = '1790-01-01 00:00:00', $endDate="9999-12-31 23:59:59", $excludingIds = array())
 	{
 		$qry = "select sum(t.value) as sum,acc.name,acc.id
 				from transaction t 
@@ -77,7 +121,6 @@ class TransactionService extends BaseService
 				group by acc.id
 				order by sum desc
 				limit $noOfItems";
-
 		$results = Dao::getResultsNative($qry,array(),PDO::FETCH_ASSOC);
 		return $results;
 	}
