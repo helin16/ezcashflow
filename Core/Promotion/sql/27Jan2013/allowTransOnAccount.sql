79be97a2dffbdfccf567cfcdb25ab591bcd0eff9
helin16@gmail.com <helin16@gmail.com@dcc96739-f24e-c616-75c6-04b6b03958bd>
2013-01-27 11:44:48 +0000

    changes:  - aded a new column called sum for the summary of values  - changed the way to get the breadcrumbs  - added a new column to flag an account as leaf accountentry

--- Core/Promotion/sql/27Jan2013/allowTransOnAccount.sql
+++ Core/Promotion/sql/27Jan2013/allowTransOnAccount.sql
@@ -1,7 +1,7 @@
 ALTER TABLE `accountentry` ADD `allowTrans` TINYINT NOT NULL DEFAULT '0' AFTER `rootId` ;
 ALTER TABLE `accountentry` ADD INDEX ( `allowTrans` );
 
-ALTER TABLE `accountentry` ADD `sum` deciaml(10,2) NOT NULL DEFAULT '0' AFTER `allowTrans`;
+ALTER TABLE `accountentry` ADD `sum` DECIMAL(10,2) NOT NULL DEFAULT '0' AFTER `allowTrans`;
 ALTER TABLE `accountentry` CHANGE `value` `value` DECIMAL( 10, 2 ) NOT NULL DEFAULT '0' , CHANGE `budget` `budget` DECIMAL( 10, 2 ) NOT NULL DEFAULT '0';
 ALTER TABLE `transaction` CHANGE `value` `value` DECIMAL( 10, 2 ) NOT NULL DEFAULT '0';
 
