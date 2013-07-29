a95731dc602f7caeb8c452f4d3f26e7a904728e0
helin16 <helin16@dcc96739-f24e-c616-75c6-04b6b03958bd>
2013-07-29 02:29:12 +0000

    rolled back to original

--- Web/protected/classes/PageAbstract/PageAbstract.raw.js
+++ Web/protected/classes/PageAbstract/PageAbstract.raw.js
@@ -1,60 +1,6 @@
 //this is the source file for the AccountList page
 var AppJs = new Class.create();
 AppJs.prototype = {
-	//the localStorage of the javascript
-	localStorage: {
-		storageKey: 'bkWEB',
-		
-		//private function: get local storage
-		getStorage: function() {
-			var tmp = {};
-			return (!localStorage[storageKey]) ? {} : $.parseJSON(localStorage[storageKey])[storageKey]
-		},
-
-		//private function: save storage
-		saveStorage: function(newData) {
-			var tmp = {};
-			if (!localStorage[storageKey])
-				localStorage[storageKey] = JSON.stringify(tmp);
-
-			tmp.current = $.parseJSON(localStorage[storageKey]);
-			tmp.current[storageKey] = newData;
-			localStorage[storageKey] = JSON.stringify(tmp.current);
-		},
-		
-		//public function: add section into localStorage
-		getData: function(sectionId) {
-			var tmp = {};
-			tmp.Storage = getStorage();
-			return (tmp.Storage[sectionId] || {});
-		},
-
-		//public function: add section into localStorage
-		addToStorage: function(sectionId, newData) {
-			var tmp = {};
-			tmp.data = getStorage();
-			tmp.data[sectionId] = newData;
-			saveStorage(tmp.data);
-			return this;
-		},
-		
-		//public funtion: removing a section from localStorage
-		removeFromStorage: function(sectionId) {
-			var tmp = {};
-			tmp.data = getStorage();
-			if (tmp.data[sectionId])
-				delete tmp.data[sectionId];
-			saveStorage(tmp.data);
-			return this;
-		},
-
-		// dangerous, this is clear the whole Bsuite Local storage
-		clearStorage: function() {
-			localStorage[storageKey] = JSON.stringify({});
-			return this;
-		}
-	},
-	
 	//constructor
 	initialize: function () {},
 	postAjax: function(callbackId, data, requestProperty) {
