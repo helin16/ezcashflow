7e40fa3688183e66a6397c66e01575c47d6bc865
lhe <lhe@bytecraft.com.au>
2014-06-17 17:22:46 +1000

    added Accounts

--- public/lib/app.js
+++ public/lib/app.js
@@ -1,4 +1,32 @@
-dpd.users.login({"username": "admin", "password": "admin"}, function(user, err) {
-  if(err) return console.log(err);
-  console.log(user);
-});
+(function($) {
+	var methods = {
+		/**
+         * Initialize ezcashflow
+         * 
+         * @param {Object} options
+         * @returns {Object[]}
+         */
+        init: function(options) {
+            var settings = $.extend({}, this.ezcashflow.defaults, options);
+            return this.each(function() {
+                var $this = $(this);
+            });
+        },
+    };
+	
+	$.fn.ezcashflow = function(method) {
+        if (methods[method]) {
+            return methods[ method ].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === 'object' || !method) {
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error('Method with name ' + method + ' does not exists for jQuery.ezcashflow');
+        }
+    };
+    /**
+     *  Plugin's default options
+     */
+    $.fn.ezcashflow.defaults = {
+        name: 'EzCashFlow'
+    };
+})(jQuery);
---
+++ resources/domains/get.js
@@ -0,0 +1,3 @@
+if(this.name.length < 5) {
+    error('The length of the domain name needs to be longer than 5');
+}
