e594912321e1ec09890f26e57ad0953978eb35c6
lhe <lhe@bytecraft.com.au>
2015-01-09 16:35:42 +1100

    added uploader

---
+++ web/protected/pages/Ajax/Controller.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * This is the Ajax Service
+ * 
+ * @package    Web
+ * @subpackage Controller
+ * @author     lhe<helin16@gmail.com>
+ *
+ */
+class Controller extends TService
+{
+    /**
+     * (non-PHPdoc)
+     * @see TService::run()
+     */
+    public function run()
+    {
+    	$results = $errors = array();
+        try 
+        {
+            $method = '_' . ((isset($this->Request['method']) && trim($this->Request['method']) !== '') ? trim($this->Request['method']) : '');
+            if(!method_exists($this, $method))
+                throw new Exception('No such a method: ' . $method . '!');
+          	
+            $results = $this->$method($_REQUEST);
+        }
+        catch (Exception $ex)
+        {
+        	$errors = $ex->getMessage();
+        }
+        $this->getResponse()->flush();
+        $this->getResponse()->appendHeader('Content-Type: application/json');
+        $this->getResponse()->write(StringUtilsAbstract::getJson($results, $errors));
+    }
+}
