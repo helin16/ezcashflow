69b385b0740a3918c1088029802b0bee03df6d5f
helin16 <helin16@localhost>
2015-04-07 23:19:39 +1000

    bug

--- core/promotion/transaction.php
+++ core/promotion/transaction.php
@@ -3,7 +3,15 @@ require_once dirname(__FILE__) . '/../main/bootstrap.php';
 
 foreach(Transaction::getAll(false) as $transtion)
 {
-	$transtion->setUpdated($transtion->getUpdated())
-		->setUpdatedBy($transtion->getUpdatedBy())
-		->save();
+	try{
+		Dao::beginTransaction();
+		$transtion->setUpdated($transtion->getUpdated())
+			->setUpdatedBy($transtion->getUpdatedBy())
+			->save();
+		Dao::commitTransaction();
+	} catch (Exception $e) {
+		Dao::rollbackTransaction();
+		var_dump($e->getMessage());
+		var_dump($e->getTraceAsString());
+	}
 }
