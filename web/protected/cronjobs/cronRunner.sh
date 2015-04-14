9a2653f30a699919df9fdc247b2d3b08b595c8de
lin <helin16@gmail.com>
2015-04-14 19:44:16 +1000

    bug

--- web/protected/cronjobs/cronRunner.sh
+++ web/protected/cronjobs/cronRunner.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 # run all the message sending 
-/usr/bin/php /var/www/magentob2b/web/cronjobs/MessageSender.php >> /tmp/message_`date +"%d_%b_%y"`.log
-# run all the product import
-/usr/bin/php /var/www/magentob2b/web/cronjobs/importOrders.php >> /tmp/orderImport.log
+/usr/bin/php /var/www/ezbk/web/cronjobs/MessageSender.php >> /tmp/message_`date +"%d_%b_%y"`.log
